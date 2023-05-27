dab5f37b268ed840725777af21171772e4c88281
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-27 20:36:24 +0100

    PUSH FUNC

--- func.c
+++ func.c
@@ -1,26 +1,6 @@
 #include "monty.h"
 
 /**
- * is_num -> Checks if a string is a number
- * @str: String
- * Return: Depend Condition
-*/
-
-int is_num(char *str)
-{
-	int i = 0;
-
-	str = strtok(str, " \n");
-	if (str[0] == '-')
-		i++;
-	for (; str[i] != '\0'; i++)
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return (-1);
-	} return (0);
-}
-
-/**
  * int_to_str -> Converts an integer to a string
  * @num: Integer
  * Return: Depend Condition
--- push.c
+++ push.c
@@ -1,6 +1,26 @@
 #include "monty.h"
 
 /**
+ * is_num -> Checks if a string is a number
+ * @str: String
+ * Return: Depend Condition
+*/
+
+int is_num(char *str)
+{
+	int i = 0;
+
+	str = strtok(str, " \n");
+	if (str[0] == '-')
+		i++;
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+	} return (0);
+}
+
+/**
  * _push -> Pushes an element to the stack
  * @stack: Stack
  * @line_number: Line Number
